6e31700a76d9b398b0c7fbc08fd2841023c00fa4
Marte Løge <marte.loge@gmail.com>
2020-05-22 11:51:05 -0400

    Update App routes with new components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { createGlobalStyle } from "styled-components";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import Landingpage from "./Landingpage/Landingpage";
+import ConversationComponent from "./Conversation/Conversation";
+import Start from "./Start/Start";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -21,6 +23,12 @@ function App() {
       <GlobalStyle />
       <Switch>
         <Route exact path="/" component={Landingpage} />
+        <Route exact path="/conversation/:uuid/start" component={Start} />
+        <Route
+          exact
+          path="/conversation/:uuid/question/:id"
+          component={ConversationComponent}
+        />
       </Switch>
     </Router>
   );
