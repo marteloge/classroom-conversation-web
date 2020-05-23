ac48abf02257df635f7c3e5f3251d53d28fc24d6
Marte Løge <marte.loge@gmail.com>
2020-05-23 16:08:42 -0400

    style: update global styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,46 @@ import Landingpage from "./Landingpage/Landingpage";
 import ConversationComponent from "./Conversation/Conversation";
 import Start from "./Start/Start";
 
+import background from "./static/background2.png";
+
 const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #303030;
-    color: #FFFFFF;
+  * {
     margin: 0;
     padding: 0;
+    font-family: "Open Sans", sans-serif;
+  }
+
+  body, html, #root {
+    width: 100vw;
+    min-height: 100vh;
+  }
+
+  body {
+    background-image: url(${background});
+    background-size: cover;
+    background-repeat: no-repeat;
+    color: #FFFFFF;
     min-width: 100vw;
     min-height: 100vh;
+    font-size: 1rem;
   }
   
   h1 {
-    font-size: 40px;
-  }`;
+    font-family: "Gloria Hallelujah", cursive;
+    font-size: 2.5rem;
+  }
+
+  button {
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    padding: 1rem;
+    box-shadow: none;
+    cursor: pointer;
+    background-color: #363334;
+    color: white;
+  }
+`;
 
 function App() {
   return (
